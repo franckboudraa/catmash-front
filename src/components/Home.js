bd12e9caac6b3027593caf6a7e08b11d5f99d178
neocydfr <franckboudraa@gmail.com>
2018-03-02 14:14:46 +0100

    added fetchCats function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+import axios from 'axios';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      error: false,
+      cats: []
+    };
+  }
+
+  componentDidMount = () => {
+    this.fetchCats();
+  };
+
+  fetchCats = async () => {
+    try {
+      const { data: { images } } = await axios.get('/cats.json'); // http://es6-features.org/#ObjectMatchingDeepMatching
+      const selectedCats = await _.chain(images) // https://lodash.com/docs
+        .shuffle()
+        .take(2)
+        .value();
+      return this.setState({
+        loading: false,
+        cats: this.state.cats.push(...selectedCats) // http://es6-features.org/#SpreadOperator
+      });
+    } catch ({ response }) {
+      return this.setState({
+        loading: false,
+        error: true
+      });
+    }
+  };
   render() {
-    return <div>Home</div>;
+    return <div>home</div>;
   }
 }
 
