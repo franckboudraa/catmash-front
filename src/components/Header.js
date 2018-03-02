244bbc755032edbe78556328caaea32566d3cf5c
neocydfr <franckboudraa@gmail.com>
2018-03-02 16:53:24 +0100

    design improvement of header menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 
+import { Menu } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
+
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <div className="mb-5">
+        <Menu attached borderless className="head-gradient" inverted widths={1}>
+          <Menu.Item className="brand">CAT MASH</Menu.Item>
+        </Menu>
+        <Menu attached widths={2}>
+          <Menu.Item as={NavLink} exact to="/">
+            Home
+          </Menu.Item>
+          <Menu.Item as={NavLink} to="/scores">
+            Scores
+          </Menu.Item>
+        </Menu>
+      </div>
+    );
   }
 }
 
