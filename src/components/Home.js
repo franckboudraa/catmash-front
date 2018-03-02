c36a39c52d09f80945c44cbaf5ef387f113a3145
neocydfr <franckboudraa@gmail.com>
2018-03-02 15:02:45 +0100

    added render methods on home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import _ from 'lodash';
 import axios from 'axios';
 
+import { Card, Loader, Message } from 'semantic-ui-react';
+
 class Home extends Component {
   constructor(props) {
     super(props);
@@ -20,22 +22,42 @@ class Home extends Component {
     try {
       const { data: { images } } = await axios.get('/cats.json'); // http://es6-features.org/#ObjectMatchingDeepMatching
       const selectedCats = await _.chain(images) // https://lodash.com/docs
-        .shuffle()
-        .take(2)
+        .shuffle() // Shuffle the array of cats
+        .take(2) // Take the first 2 cats
         .value();
-      return this.setState({
-        loading: false,
-        cats: this.state.cats.push(...selectedCats) // http://es6-features.org/#SpreadOperator
-      });
+      return this.setState(prevState => ({
+        cats: [...selectedCats], // http://es6-features.org/#SpreadOperator
+        loading: false
+      }));
     } catch ({ response }) {
       return this.setState({
-        loading: false,
-        error: true
+        error: true,
+        loading: false
       });
     }
   };
+  renderError = () => {
+    return (
+      <Message negative>
+        <Message.Header>Meeow!</Message.Header>
+        <p>An error occured while loading cats!</p>
+      </Message>
+    );
+  };
+
+  renderCats = () => {
+    const { cats } = this.state;
+    return cats.map(cat => <Card color="red" key={cat.id} image={cat.url} />);
+  };
   render() {
-    return <div>home</div>;
+    const { loading, error } = this.state;
+    return (
+      <div>
+        {loading ? <Loader active inline /> : null}
+        {error ? this.renderError() : null}
+        {!loading && !error ? this.renderCats() : null}
+      </div>
+    );
   }
 }
 
