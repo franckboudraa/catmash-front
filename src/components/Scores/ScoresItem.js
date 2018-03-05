a212e5f7c4c88b4be5a6119ddb91624989edfc2c
neocydfr <franckboudraa@gmail.com>
2018-03-05 18:53:32 +0100

    improved styling on scores page with ranking

--- src/components/Scores/ScoresItem.js
+++ src/components/Scores/ScoresItem.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 import { Card, Image, Icon } from 'semantic-ui-react';
 
-const ScoresItem = ({ url, score }) => {
+const ScoresItem = ({ url, score, pos }) => {
+  let position;
+  let trophyColor;
+  switch (pos) {
+    case 1:
+      position = 'The Cuttiest';
+      trophyColor = 'yellow';
+      break;
+    case 2:
+      position = 'The Challenger';
+      trophyColor = 'grey';
+      break;
+    case 3:
+      position = 'Very Cute Too!';
+      trophyColor = 'brown';
+      break;
+    default:
+      position = '';
+  }
   return (
     <Card>
       <Image
@@ -14,7 +32,19 @@ const ScoresItem = ({ url, score }) => {
         style={{ width: '100%' }}
       />
       <Card.Content>
-        Cuteness: <strong>{score}</strong> <Icon name="paw" />
+        <p>
+          <span className="righteous font-185rem">
+            {pos >= 1 && pos <= 3 ? (
+              <Icon color={trophyColor} name="trophy" />
+            ) : (
+              <strong>#{pos}</strong>
+            )}{' '}
+            {position}
+          </span>
+        </p>
+        <p>
+          Cuteness: <strong>{score}</strong> <Icon name="paw" />
+        </p>
       </Card.Content>
     </Card>
   );
