427faa1298acb1870e49cde0cdcbe601d71dd59f
neocydfr <franckboudraa@gmail.com>
2018-03-05 16:03:58 +0100

    header from class to stateless comp

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,34 +3,28 @@ import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="mb-5">
-        <Menu attached borderless className="head-gradient" inverted widths={1}>
-          <Menu.Item className="righteous font-4rem">CAT MASH</Menu.Item>
-        </Menu>
-        <Menu attached inverted color="red" widths={2}>
-          <Menu.Item
-            as={NavLink}
-            exact
-            to="/"
-            className="righteous font-175rem"
-            size="massive"
-          >
-            Arena
-          </Menu.Item>
-          <Menu.Item
-            as={NavLink}
-            to="/scores"
-            className="righteous font-175rem"
-          >
-            Rankings
-          </Menu.Item>
-        </Menu>
-      </div>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <div className="mb-5">
+      <Menu attached borderless className="head-gradient" inverted widths={1}>
+        <Menu.Item className="righteous font-4rem">CAT MASH</Menu.Item>
+      </Menu>
+      <Menu attached inverted color="red" widths={2}>
+        <Menu.Item
+          as={NavLink}
+          exact
+          to="/"
+          className="righteous font-175rem"
+          size="massive"
+        >
+          Arena
+        </Menu.Item>
+        <Menu.Item as={NavLink} to="/scores" className="righteous font-175rem">
+          Rankings
+        </Menu.Item>
+      </Menu>
+    </div>
+  );
+};
 
 export default Header;
