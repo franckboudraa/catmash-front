a699eeef80f40e150a81d3256a604975ec66821b
neocydfr <franckboudraa@gmail.com>
2018-03-05 17:59:18 +0100

    remove hover effect on card from scores page

--- src/components/Scores/ScoresItem.js
+++ src/components/Scores/ScoresItem.js
@@ -4,7 +4,7 @@ import { Card, Image, Icon } from 'semantic-ui-react';
 
 const ScoresItem = ({ url, score }) => {
   return (
-    <Card link>
+    <Card>
       <Image bordered circular className="thumbnail" as="img" src={url} />
       <Card.Content>
         Cuteness: <strong>{score}</strong> <Icon name="paw" />
